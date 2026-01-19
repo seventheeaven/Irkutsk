cd595e9e0eb7752f568dce80e790d5f3882f0e47
Вероника Стреж <strezzz@MacBook-Air-Veronika.local>
2026-01-19 23:29:04 +0800

    Update auth page text: change to 'Сохраняйте места и делитесь своими публикациями' with line break

--- src/pages/MyCollectionsPage.tsx
+++ src/pages/MyCollectionsPage.tsx
@@ -215,7 +215,7 @@ export const MyCollectionsPage = () => {
         <div className="my-collections-page__create-account">
           <div className="my-collections-page__create-account-gradient">
             <p className="my-collections-page__create-account-text">
-              Сохраняйте места и создавайте свои публикации
+              Сохраняйте места и<br />делитесь своими публикациями
             </p>
           </div>
           <div className="my-collections-page__create-account-content">
