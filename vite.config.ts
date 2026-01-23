fbecc654f480fd86539344c53eded01ea4f22355
Вероника Стреж <strezzz@MacBook-Air-Veronika.local>
2026-01-23 16:53:42 +0800

    Restore to 3a8e1ae configuration (before mobile loading issues started)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    host: true,
+    port: 3000
+  },
   build: {
-    outDir: 'dist'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
   }
 })
